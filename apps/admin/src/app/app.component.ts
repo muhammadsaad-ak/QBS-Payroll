48c745d76cbc2e42280b120d834c03773875de08
Muhammad Saad Ali Khan <muhammad.saad@qbsco.net>
2025-04-24 15:14:23 +0500

    Refactor AppComponent to use standalone feature and integrate CommonModule. Update sign-in component layout for improved responsiveness and styling with Tailwind CSS.

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [ RouterModule],
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [ RouterModule, CommonModule],
 })
 export class AppComponent {
   title = 'admin';
